1ad8d3b9e56abd4c8cac36813ff8a3cfdc3959cc
Heilsa <katushenko.a@mail.ru>
2025-03-23 21:49:37 +0700

    Rework homework8

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
         int year = 2025;
         checkLeapYear(year);
     }
+
     public static void checkLeapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год — високосный год");
@@ -13,43 +12,38 @@ public class Main {
             System.out.println(year + " год — невисокосный год");
         }
         System.out.println("Задача 2");
-        class AppInstallationSuggestion {
-            public static void main(String[] args) {
-                int clientDeviceYear = 2018;
-                int osType = 0;
-                suggestAppInstallation(osType, clientDeviceYear);
-            }
-            public static void suggestAppInstallation(int osType, int clientDeviceYear) {
-                int currentYear = LocalDate.now().getYear();
-                if (clientDeviceYear < 2015) {
-                    if (osType == 0) {
-                        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                    } else {
-                        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                    }
-                } else {
-                    System.out.println("Установите обычную версию приложения");
-                }
-            }
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        recommendVersion(clientOS, clientDeviceYear);
+    }
+
+    public static void recommendVersion(int clientOS, int clientDeviceYear) {
+        String osType;
+        if (clientOS == 1) {
+            osType = "iOS";
+        } else {
+            osType = "Android";
+        }
+        if (clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для " + osType + " по ссылке");
+        } else {
+            System.out.println("Установите обычную версию приложения для " + osType + " по ссылке");
         }
         System.out.println("Задача 3");
-        class CardDelivery {
-            public static void main(String[] args) {
-                int deliveryDistance = 95;
-                int deliveryDays = calculateDeliveryDays(deliveryDistance);
-                System.out.println("Потребуется дней: " + deliveryDays);
-            }
-            public static int calculateDeliveryDays(int distance) {
-                if (distance <= 20) {
-                    return 1;
-                } else if (distance <= 60) {
-                    return 2;
-                } else if (distance <= 100) {
-                    return 3;
-                } else {
-                    return 0;
-                }
-            }
+        int deliveryDistance = 95;
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+        System.out.println("Потребуется дней: " + deliveryDays);
+    }
+
+    public static int calculateDeliveryDays(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
+        } else {
+            return 0;
         }
     }
 }
